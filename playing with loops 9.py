9d6ce8c57a5180fa8a8db16f297e6bba824ed1b7
techindiesoftgaming <62423720+techindiesoftgaming@users.noreply.github.com>
2021-01-18 00:23:16 +0530

    Update playing with loops 9.py
    perfect number

--- playing with loops 9.py
+++ playing with loops 9.py
@@ -3,5 +3,12 @@ a perfect number is a positive integer which is equal to its sum of its divisors
 
 
 """
-
-
+n = int(input("Enter a number: "))
+summ = 0
+for i in range(1, n):
+    if n % i == 0:
+        summ += i
+if summ == n:
+    print(n, "is a perfect number")
+else:
+    print("its not a perfect number")
