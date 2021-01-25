7bcc2e714b0c6f7c729462af1d6e713427d16f1e
techindiesoftgaming <62423720+techindiesoftgaming@users.noreply.github.com>
2021-01-25 23:30:47 +0530

    changes done

--- playing with loops 6.py
+++ playing with loops 6.py
@@ -6,12 +6,7 @@
   *
 
 """
-for i in range(1,4):
-    print(" "*(4-i)+"* "*i)
-for i in range(2,0,-1):
-    print(" "*(3-i)+" *"*i)
-
-
-
-
-
+for i in range(1, 4):
+    print(" " * (4 - i) + "* " * i)
+for i in range(2, 0, -1):
+    print(" " * (3 - i) + " *" * i)
--- playing with loops7.py
+++ playing with loops7.py
@@ -8,5 +8,5 @@
 
 
 """
-for i in range(1,6):
-    print("1"*i)
+for i in range(1, 6):
+    print("1" * i)
