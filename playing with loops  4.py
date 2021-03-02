d15d40344126e1a103f164ef244e666eac00f21b
techindiesoftgaming <62423720+techindiesoftgaming@users.noreply.github.com>
2021-03-02 22:33:34 +0530

    Reformatted the file

--- playing with loops  4.py
+++ playing with loops  4.py
@@ -12,10 +12,10 @@ ABCD
 0123
 
 """
-str1='ABCD'
-str2='PQR'
+str1 = 'ABCD'
+str2 = 'PQR'
 for i in range(4):
-     print(str1[ : i+1 ] + str2[ i : ])
+    print(str1[: i + 1] + str2[i:])
 """this is the answer i got from some were,hmm 
 i do have to know the string part 
 for this type of pogram"""
